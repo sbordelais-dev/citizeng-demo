be21572d732477583ff4512f4d5243683661bd60
Lord Hadder <31163409+LordHadder@users.noreply.github.com>
2019-08-15 13:03:49 +0200

    Update citizeng-demo.js

--- citizeng-demo.js
+++ citizeng-demo.js
@@ -3,7 +3,7 @@ var citizeng  = require('citizeng');
 var path      = require('path');
 
 // Initialize the server.
-citizeng.init(3030, "root", "root", "rootAtroot.com", __dirname);
+citizeng.init(3030, "root", "root", "root@root.com", __dirname);
 
 // Declare main page route.
 citizeng.get  ( "/"
